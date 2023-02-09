dc62af7d968b215af25d321f12263f0fe47440ae
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-09 21:42:24 +0300

    minimum opearations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
+"""
+Main file for testing
+"""
 
+
+def minOperations(n):
+    '''
+    min operations
+    Args:
+      n:
+    Returns:
+      n is impossible to achieve, return 0
+    '''
+    if n <= 0:
+        return 0
+    operations = 0
+    i = 2
+    while i <= n:
+        while n % i == 0:
+            operations += i
+            n /= i
+        i += 1
+    return operations

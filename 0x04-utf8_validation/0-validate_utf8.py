d2fa9692df5f66740d70763857af3088a728ecd6
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-23 23:53:42 +0300

    Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,35 +1,29 @@
-#!/usr/bin/pyhon3
-def validUTF8(data):
-    # Number of bytes in the current UTF-8 character
-    num_bytes = 0
-    
-    # Loop through each integer in the data list
-    for num in data:
-        # Get the binary representation of the current byte
-        binary = format(num, '#010b')[-8:]
-        
-        # If the current byte is part of a UTF-8 character...
-        if num_bytes > 0:
-            # If the current byte does not start with the bit pattern "10", it's not a valid continuation byte
-            if not binary.startswith("10"):
-                return False
-            
-            # Decrement the number of bytes remaining in the current UTF-8 character
-            num_bytes -= 1
-        else:
-            # Count the number of bytes in the current UTF-8 character based on the leading bits
-            if binary.startswith("0"):
-                num_bytes = 0
-            elif binary.startswith("110"):
-                num_bytes = 1
-            elif binary.startswith("1110"):
-                num_bytes = 2
-            elif binary.startswith("11110"):
-                num_bytes = 3
-            else:
-                # Invalid leading bits
-                return False
-    
-    # If we have any remaining bytes in the current UTF-8 character, it's not a valid encoding
-    return num_bytes == 0
+#!/usr/bin/python3
+"""
+UTF-8 Validation
+"""
 
+
+def validUTF8(data):
+    """
+    data: a list of integers
+    Return: True if data is a valid UTF-8
+    encoding, else return False
+    """
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            byte_count -= 1
+    return byte_count == 0

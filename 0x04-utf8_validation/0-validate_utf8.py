34d7a900d2130aaf54325219342d192478f2bbf0
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-02-23 23:46:13 +0300

    UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,23 +1,35 @@
 #!/usr/bin/pyhon3
-"""
-Will validate UTF8 module
-"""
 def validUTF8(data):
-    """validUTF8
-    Determines if given data set represents a valid UTF-8 encoding.
-    """
+    # Number of bytes in the current UTF-8 character
+    num_bytes = 0
     
-    valid = 0
-    for value in data:
-        byte = value & 255
-        if valid:
-            if byte >> 6 != 2:
+    # Loop through each integer in the data list
+    for num in data:
+        # Get the binary representation of the current byte
+        binary = format(num, '#010b')[-8:]
+        
+        # If the current byte is part of a UTF-8 character...
+        if num_bytes > 0:
+            # If the current byte does not start with the bit pattern "10", it's not a valid continuation byte
+            if not binary.startswith("10"):
                 return False
-            valid -= 1
-            continue
-        while (1 << abs(7 - valid)) & byte:
-            valid += 1
-        if valid == 1 or valid > 4:
-            return False
-        valid = max(valid - 1, 0)
-    return valid == 0
+            
+            # Decrement the number of bytes remaining in the current UTF-8 character
+            num_bytes -= 1
+        else:
+            # Count the number of bytes in the current UTF-8 character based on the leading bits
+            if binary.startswith("0"):
+                num_bytes = 0
+            elif binary.startswith("110"):
+                num_bytes = 1
+            elif binary.startswith("1110"):
+                num_bytes = 2
+            elif binary.startswith("11110"):
+                num_bytes = 3
+            else:
+                # Invalid leading bits
+                return False
+    
+    # If we have any remaining bytes in the current UTF-8 character, it's not a valid encoding
+    return num_bytes == 0
+
